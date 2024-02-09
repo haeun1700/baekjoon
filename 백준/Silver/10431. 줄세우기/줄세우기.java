940779189ed16aa205ebb16b454a75b5bdf6bcf8
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-02-09 16:19:34 +0900

    [Silver V] Title: 줄세우기, Time: 264 ms, Memory: 17272 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,31 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-class Main
-{
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int P = sc.nextInt();
-        while (P-- > 0) {
-            int T = sc.nextInt();
-            int[] h = new int[20];
-            for (int i = 0; i < 20; i++)
-                h[i] = sc.nextInt();
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token;
+        int p = Integer.parseInt(br.readLine());
+
+        while(p-- > 0){
+            token = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(token.nextToken());
+            int[] arr= new int[20];
+            for(int i = 0; i< 20; i++){
+                arr[i] = Integer.parseInt(token.nextToken());
+            }
 
             int cnt = 0;
-            for (int i = 0; i < 20; i++) {
-                for (int j = 0; j < i; j++) {
-                    if (h[j] > h[i]) {
-                        int myH = h[i];
-                        for (int k = i; k > j; k--) {
-                            h[k] = h[k - 1];
+            int[] sorted = new int[20];
+            for(int  i = 0; i< 20; i++){
+                boolean find = false;
+                for(int j = 0; j<i; j++){
+                    if(sorted[j] > arr[i]){
+                        find = true;
+                        for(int k = i-1; k >= j; k--){
+                            sorted[k+1] = sorted[k];
                             cnt++;
                         }
-                        h[j] = myH;
+                        sorted[j] = arr[i];
                         break;
                     }
                 }
+                if(!find) sorted[i] = arr[i];
             }
-            System.out.println(T + " " + cnt);
+            System.out.println(t + " " + cnt);
         }
     }
 }
