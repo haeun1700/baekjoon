395a6a9799e8f17148d0bce6a2b31c91e47b22a2
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-03-22 18:33:12 +0900

    [Silver I] Title: 회의실 배정, Time: 4152 ms, Memory: 51900 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,15 @@
+n = int(input())
+meetings = []
+# 끝나는 시간을 기준으로 정렬하기
+for _ in range(n):
+    start, end = map(int, input().split())
+    meetings.append((start, end))
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+count = 1
+end = meetings[0][1]
+for i in range(1,n):
+    if meetings[i][0] >= end:
+        end = meetings[i][1]
+        count += 1
+print(count)
