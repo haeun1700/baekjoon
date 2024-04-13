bb17525c702d9d1589a96824135c31556df1cd64
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-04-13 20:32:27 +0900

    [Silver II] Title: 생태학, Time: 468 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.py
@@ -0,0 +1,19 @@
+import sys
+dic = {}
+while True:
+    n = sys.stdin.readline().strip()
+    # if not n : break
+    if n == "":
+        break
+    elif n not in dic:
+        dic[n] = 1
+    else:
+        dic[n] += 1
+
+dic_len = sum(dic.values())
+arr = sorted(dic.keys())
+
+for i in arr:
+    if i == "": break
+    num = (dic[i] / dic_len) * 100
+    print(i, format(num, '.4f'))
