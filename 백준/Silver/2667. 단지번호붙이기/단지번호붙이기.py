4059e229f50787a236ae3d4724a8b74ab0e3c971
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-05-16 18:53:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 48 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,47 +1,35 @@
-import sys
-from collections import deque
+dx = [0,0,1,-1]
+dy = [-1,1,0,0]
+n = int(input())
 
-input = sys.stdin.readline
-
-N = int(input())
-
-graph = []  # 입력받을 그래프를 담을 리스트 선언
-result = []  # 결과를 담을 리스트 선언
+visited = [[0] * n for _ in range(n)]
+arr = []
 count = 0
+graph = []
 
-for _ in range(N):
+for _ in range(n):
     graph.append(list(map(int, input().rstrip())))
 
-# 한 점을 기준으로 (위 아래 왼쪽 오른쪽) 으로 한칸 씩 이동할 좌표 설정
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-
-def dfs(x, y):
+def dfs(x,y):
+    visited[x][y] = 1
     global count
-
-    if x < 0 or x >= N or y < 0 or y >= N:
-        return
-
-    if graph[x][y] == 1:
-        count += 1
-        graph[x][y] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+    count += 1
+    for i in range(4):
+        nx = x + dy[i]
+        ny = y + dx[i]
+        if nx < 0 or ny < 0 or nx >= n or ny >= n:
+            continue
+        if graph[nx][ny] == 1 and visited[nx][ny] == 0:
             dfs(nx, ny)
 
 
-# 그래프의 원소가 1일때만 dfs로 집을 방문한다.
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 1:
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 and visited[i][j] == 0:
             dfs(i, j)
-            result.append(count)
+            arr.append(count)
             count = 0
 
-result.sort()  # 오름차순으로 정렬
-
-print(len(result))  # 총 단지수 출력
-for k in result:  # 각 단지마다 집의 수 출력
-    print(k)
+print(len(arr))
+for i in sorted(arr):
+    print(i)
