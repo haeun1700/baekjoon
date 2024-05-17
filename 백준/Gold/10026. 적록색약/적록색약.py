76be53b2ec2d65fcd168fd70832c0d8c0ce50719
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-05-17 17:08:02 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,47 +1,49 @@
 import sys
-from queue import Queue
-sys.setrecursionlimit(10**6)
-input = lambda : sys.stdin.readline().rstrip()
+from collections import deque
 
-N = int(input())
-graph = []
-q = Queue()
-visited = [[0] * N for _ in range(N)]
-for _ in range(N):
-    graph.append(list(map(str, input())))
+input = lambda : sys.stdin.readline()
 
-dx = [0,0,-1,1] #x를 가로, y를 세로
-dy = [-1,1,0,0]
-def dfs(i, j):
-    visited[i][j] = 1
-    color = graph[i][j]
-    for k in range(4):
-        nx = dx[k] + j
-        ny = dy[k] + i
-        if nx < 0 or ny < 0 or nx >= N or ny >= N:
-            continue
-        if visited[ny][nx] == 0 and graph[ny][nx] == color:
-            dfs(ny, nx)
+q = deque()
+n = int(input())
+graph = [list(input()) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
 
-# #상하좌우가 빨 파 그 일때
-count = 0
-for i in range(N):
-    for j in range(N):
-        if visited[i][j] == 0:
-            dfs(i,j)
-            count +=1
+dx = [-1, 1, 0,0]
+dy = [0,0,-1,1]
 
-visited = [[0] * N for _ in range(N)]
-count_two = 0
-for i in range(N):
-    for j in range(N):
+def bfs(x,y):
+    q = deque([[x,y]])
+    visited[x][y] = 1
+    color = graph[x][y]
+
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
+            if visited[nx][ny] == 0 and graph[nx][ny] == color:
+                q.append([nx,ny])
+                visited[nx][ny] = 1
+
+def get_cnt():
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == 0:
+                bfs(i, j)
+                cnt += 1
+
+    return cnt
+
+
+print(get_cnt(), end=' ')
+visited = [[0] * n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
         if graph[i][j] == 'R':
             graph[i][j] = 'G'
 
-for i in range(N):
-    for j in range(N):
-        if visited[i][j] == 0:
-            dfs(i,j)
-            count_two +=1
-
-print(count, count_two)
+print(get_cnt())
