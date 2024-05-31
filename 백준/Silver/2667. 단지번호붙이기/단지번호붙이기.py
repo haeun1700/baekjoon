125797c0b7746c9e32aaa2cf4fc3ab22f9244925
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-05-31 14:14:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,32 +1,30 @@
-dx = [0,0,1,-1]
-dy = [-1,1,0,0]
-n = int(input())
+dx = [-1,1,0,0]  #x세로, y가로
+dy = [0,0,-1,1]  # 상하좌우
 
+
+n = int(input())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
 visited = [[0] * n for _ in range(n)]
 arr = []
 count = 0
-graph = []
-
-for _ in range(n):
-    graph.append(list(map(int, input().rstrip())))
 
 def dfs(x,y):
     visited[x][y] = 1
     global count
     count += 1
     for i in range(4):
-        nx = x + dy[i]
-        ny = y + dx[i]
-        if nx < 0 or ny < 0 or nx >= n or ny >= n:
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >=n or ny >= n:
             continue
         if graph[nx][ny] == 1 and visited[nx][ny] == 0:
-            dfs(nx, ny)
+             dfs(nx,ny)
 
 
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1 and visited[i][j] == 0:
-            dfs(i, j)
+            dfs(i,j)
             arr.append(count)
             count = 0
 
