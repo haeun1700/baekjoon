f6205055b769417c82ff8b5d6fd0f7e137be144c
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-06-07 19:11:33 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 844 ms, Memory: 157604 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,29 +1,16 @@
-n = int(input())
-A = list(map(int,input().split()))
-result = [A[0]]
+import sys
+from bisect import bisect_left
 
-for i in A[1:]:
-    start, end = 0,len(result)
-    
-    # 현재 수가 결과 수열의 마지막보다 크다면 
-    # 그냥 append 해주고 끝내면 된다.
-    if i > result[-1]:
-        result.append(i)
-        continue
-    
-    # 이분 탐색으로 현재 수가 들어갈 적절한 위치를 찾는다.
-    while(start < end):
-        mid = (start + end)//2
-        
-        if result[mid] < i:
-            start = mid + 1
-        else:
-            end = mid
-    
-    # 들어갈 자리에 있는 수가 i보다 크거나 같다면 i로 교체
-    # 작다면 굳이 바꿀 필요가 없으니 다음 수를 i로 교체해준다.
-    if result[mid] >= i:
-        result[mid] = i
+read = sys.stdin.readline
+
+n = int(read())
+num_list = list(map(int, read().split()))
+
+sorted_list = [0]
+for num in num_list:
+    if sorted_list[-1] < num:
+        sorted_list.append(num)
     else:
-        result[mid+1] = i
-print(len(result))
+        sorted_list[bisect_left(sorted_list, num)] = num
+
+print(len(sorted_list) - 1)
