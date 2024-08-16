bea2d60fef2b7bd630be2f471ed8b730ec8a4a7e
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-08-16 16:17:54 +0900

    [Silver I] Title: 신입 사원, Time: 3340 ms, Memory: 44580 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,19 +1,23 @@
 import sys
-
-input = lambda :sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
 t = int(input())
-a = []
-b = []
+
 for _ in range(t):
+    candidates = []
     n = int(input())
-    people = [list(map(int, input().split())) for _ in range(n)]
-    people.sort(key=lambda x: x[0])
-    cnt = 1
-    temp = people[0][1]
-    for i in range(1,n):
-        if people[i][1] < temp:
-                # 어차피 면접 기존 temp에 있는 점수는 비교해도 못이기기 때문에 temp를 리뉴얼하여 얘를 이기면 이기게 됨.
-                temp = people[i][1]
-                cnt += 1
-    print(cnt)
+    for _ in range(n):
+        s,m = map(int, input().split())
+        candidates.append((s,m))
+
+    candidates.sort()
+
+    top_ranking = 1e9
+    count = 0
+    
+    for i in range(n):
+        if candidates[i][1] < top_ranking:
+            count += 1
+            top_ranking = candidates[i][1]
+
+    print(count)
