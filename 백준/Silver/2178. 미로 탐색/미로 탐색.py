01f758899f3bec36d6abf123f5e4d2545d9aed97
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-10-05 20:19:47 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,30 @@
+import sys
 from collections import deque
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-count = 0
+input = lambda: sys.stdin.readline().rstrip()
 
-def bfs(x,y):
-    visited[x][y] = 1
-    q = deque()
-    q.append((x,y))
+
+def maze_search(rows, cols, maze):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    q = deque([(0, 0)])
+
     while q:
-        x,y = q.popleft()
+        x, y = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx <0 or ny <0 or nx >=n or ny>= m:
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if nx < 0 or nx >= rows or ny < 0 or ny >= cols or maze[nx][ny] != 1:
                 continue
-            if graph[nx][ny] == 1 and visited[nx][ny] ==0:
-                q.append((nx,ny))
-                visited[nx][ny] = 1
-                graph[nx][ny] = graph[x][y] + 1
+
+            q.append((nx, ny))
+            maze[nx][ny] = maze[x][y] + 1
 
 
-bfs(0,0)
-print(graph[n-1][m-1])
+    return maze[rows-1][cols-1]
+
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+print(maze_search(n, m, arr))
