c4c8e67d0d298856d82448ac463b1e9c82cf6a4a
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2024-12-15 16:29:00 +0900

    [Silver II] Title: 최소 힙, Time: 116 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,28 +1,12 @@
-# import sys
-# input = sys.stdin.readline
-#
-# n = int(input())
-# # 시작 시간, 종료 시간 정렬
-# # 정렬된 리스트 순서대로 실행 if 끝나는 시간 > 시작시간 강의실 하나 추가
-#
-# 2 14   1
-# 3  8   1  12 18  20 25
-# 6  20   1
-# 6  27   1
-# 7  13   1  15  21
-
-import heapq
-import sys
+import heapq, sys
 
 input = lambda: sys.stdin.readline()
 n = int(input())
-h= []
+arr = []
 
-for i in range(n):
+for _ in range(n):
     num = int(input())
-    if num == 0:
-        if len(h) == 0: print(0)
-        else:
-            print(heapq.heappop(h))
+    if num != 0:
+        heapq.heappush(arr, num)
     else:
-        heapq.heappush(h, num)
+        print(heapq.heappop(arr) if len(arr) > 0 else 0)
