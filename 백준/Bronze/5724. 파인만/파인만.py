fa891c0c0bee822356b4c09372eeed144c9a42d6
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-02-19 00:02:35 +0900

    [Bronze III] Title: 파인만, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5724. 파인만/파인만.py
+++ 백준/Bronze/5724. 파인만/파인만.py
@@ -2,9 +2,8 @@ while True:
     n = int(input())
     if n == 0:
         break
-
     answer = 0
-    for i in range(n):
-        for j in range(n):
-            answer += min(n-i, n-j)
+
+    for i in range(n+1):
+        answer += i*i
     print(answer)
