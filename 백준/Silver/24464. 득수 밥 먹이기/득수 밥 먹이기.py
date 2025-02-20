793ef62e9c3e2de24cd46fe217f3672d7d541c3e
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-02-21 03:24:32 +0900

    [Silver I] Title: 득수 밥 먹이기, Time: 200 ms, Memory: 134476 KB -BaekjoonHub

--- 백준/Silver/24464. 득수 밥 먹이기/득수 밥 먹이기.py
+++ 백준/Silver/24464. 득수 밥 먹이기/득수 밥 먹이기.py
@@ -1,14 +1,16 @@
 MOD = 1000000007
-
 n = int(input())
 dp = [[0,0,0,0,0] for i in range(n+1)]
 
 dp[1] = [1,1,1,1,1]
 for i in range(2, n+1):
-    dp[i][0] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][3] + dp[i-1][4]) % MOD
-    dp[i][1] = (dp[i-1][0] + dp[i-1][3] + dp[i-1][4]) % MOD
-    dp[i][2] = (dp[i-1][0] + dp[i-1][4]) % MOD
-    dp[i][3] = (dp[i-1][0] + dp[i-1][1]) % MOD
-    dp[i][4] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD
+    for j in range(5):
+        for k in range(5):
+            if j == 0 and k == 0:
+                continue
+            if j != 0 and k != 0 and abs(j-k) <= 1:
+                continue
+            dp[i][j] += dp[i-1][k]
+            dp[i][j] %= MOD
 
 print(sum(dp[n]) % MOD)
