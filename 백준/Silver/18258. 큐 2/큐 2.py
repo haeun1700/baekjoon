583cce9a2de08b1f4345cbc93644f6c0348ca1e4
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-02-26 02:02:27 +0900

    [Silver IV] Title: 큐 2, Time: 808 ms, Memory: 253720 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,21 +1,21 @@
 import sys
 from collections import deque
 
-input = lambda: sys.stdin.readline().rstrip()
 n = int(input())
+input = lambda: sys.stdin.readline().split()
 dq = deque()
 
 for _ in range(n):
     cmd = input()
-    if cmd.split(" ")[0] == "push":
-        dq.append(cmd.split(" ")[1])
-    elif cmd == "pop":
+    if cmd[0] == "push":
+        dq.append(cmd[1])
+    elif cmd[0] == "pop":
         print(dq.popleft()) if len(dq) != 0 else print(-1)
-    elif cmd == "size":
+    elif cmd[0] == "size":
         print(len(dq))
-    elif cmd == "empty":
+    elif cmd[0] == "empty":
         print(0) if len(dq) != 0 else print(1)
-    elif cmd == "front":
+    elif cmd[0] == "front":
         print(dq[0]) if len(dq) != 0 else print(-1)
-    elif cmd == "back":
+    elif cmd[0] == "back":
         print(dq[-1]) if len(dq) != 0 else print(-1)
