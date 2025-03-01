9fa82a5966ed2c0936d049908490ce31df14d4da
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-03-02 07:30:12 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 152 ms, Memory: 114772 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,15 +1,30 @@
-from bisect import bisect_left
-
+# a -> b 먹는다, 즉 A가 먹을 수 있는 쌍이 몇개인지?
 t = int(input())
+
+def two_pointer(a,b, n, m):
+    a.sort() # 1 1 3 7 8
+    b.sort() # 1 3 6
+
+    start = 0
+    end = 0
+    cnt = 0
+
+    while start < n:
+        if end == m:
+            cnt += end
+            start += 1
+        else:
+            if a[start] > b[end]:
+                end += 1
+            else:
+                start += 1
+                cnt += end
+
+    return cnt
+
+
 for _ in range(t):
     n, m = map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    
-    a.sort()
-    b.sort()
-    res = 0
-
-    for i in a:
-        res += bisect_left(b,i) # i가 b에 들어갈 수 있는 왼쪽 자리
-    print(res)
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    print(two_pointer(A,B, n, m))
