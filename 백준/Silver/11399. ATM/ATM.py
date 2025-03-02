733ad7618816c044aef3b637e96b654f49cc6880
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-03-02 17:08:23 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,19 +1,17 @@
+# i번 사람이 돈을 인출하는데 걸리는 시간 Pi분, 필요한 시간 최솟값 구하기
+# 1 2 3 4 5
+# 3 1 4 3 2
+# 그리디, 18분 소요
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline()
 
 n = int(input())
-time = list(map(int, input().split()))
-time.sort()
-
-time_sum = 0
-# 1 2 3 3 4
-count = 1
+P = list(map(int, input().split())) #
+# 앞 사람이 빨리 끝날수록 필요한 시간이 최솟값이 된다 -> 정렬
+P.sort()
+res = 0
 for i in range(n):
-    if i == 0:
-        time[i] = time[i]
+    res += P[i]
+    P[i] = res
 
-    else:
-        time[i] = time[i] + time[i-1]
-
-total = sum(time)
-print(total)
+print(sum(P))
