ba4702f1b485a3217556d7049d67e32386eb3a37
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-03-17 13:52:08 +0900

    [Silver III] Title: 통계학, Time: 356 ms, Memory: 55368 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,34 +1,22 @@
-# n개의 수를 대표하는 기본 통계값
-# 4가지 통계값 구하기
+from collections import Counter
 import sys
-input = lambda :sys.stdin.readline()
-n = int(input())
 
-# n개의 정수
-arr = [int(input()) for i in range(n)]
-arr.sort()
-dic = {}
-for i in arr:
-    if i not in dic:
-        dic[i] = 1
-    else:
-        dic[i] += 1
-dic_sorted = sorted(dic.items(), key=lambda x: x[1], reverse=True)
-# max_value = dic_sorted[0][0]
-# second_value = -1e9
+input = sys.stdin.readline
 
+N = int(input())
+Li = sorted(int(input()) for _ in range(N))
 
-result = max(dic, key=dic.get)
-print(round(sum(arr) / n)) # 산술
-print(arr[(n//2)]) # 중앙
+tmp = Counter(Li)
+max_tmp = max(tmp.values())
+mode = [key for key, value in tmp.items() if value == max_tmp]
 
-if n == 1:
-    print(dic_sorted[0][0])
+print(round(sum(Li) / N))  # 평균값
+print(Li[N // 2])  # 중앙값
+
+if len(mode) > 1:  # 최빈값
+    print(mode[1])
+
 else:
-    if dic_sorted[0][1] == dic_sorted[1][1]:
-        print(dic_sorted[1][0])
-    else:
-        print(dic_sorted[0][0])
-# print(max_value if n == 1 else max(max_value, second_value))
-print(arr[-1] - arr[0])
+    print(mode[0])
 
+print(max(Li) - min(Li))  # 범위
