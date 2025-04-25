d79af7ab3f155b64f74cb3453dc13425caab7b2b
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-04-25 17:03:26 +0900

    [Silver I] Title: 정수 삼각형, Time: 124 ms, Memory: 42596 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,17 +1,9 @@
 n = int(input())
-dp = [[0] * n for _ in range(n)]
-a = [list(map(int, input().split())) for _ in range(n)]
-dp[0][0] = a[0][0]
+dp = [[0] * (n+1) for _ in range(n+1)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-def dp_triangle():
-    for i in range(1, n):
-        for j in range(0, i+1):
-            if j == 0:
-                dp[i][j] = dp[i-1][j] + a[i][j]
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + a[i][j]
+for i in range(1, n+1):
+    for j in range(1, len(graph[i-1])+1):
+        dp[i][j] = graph[i-1][j-1] + max(dp[i-1][j], dp[i-1][j-1])
 
-    return max(dp[n-1])
-
-
-print(dp_triangle())
+print(max(dp[n]))
