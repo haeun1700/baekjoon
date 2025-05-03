f1a6a2b31c33d4ea5b83be9af85626bb51e78522
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-05-03 12:34:04 +0900

    [Silver III] Title: 빈도 정렬, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,18 +1,14 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
 n, c = map(int, input().split())
 arr = list(map(int, input().split()))
-dic = dict()
+dic = {}
 
 for i in arr:
     if i not in dic:
-        dic[i] = 0
+        dic[i] = 1
+    elif i in dic:
+        dic[i] += 1
 
-    dic[i] += 1
-
-dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
-
-for key, value in dic:
-    for i in range(value):
-        print(str(key) + '', end=' ')
+sorted_dic = sorted(dic.items(), key = lambda x: x[1], reverse=True)
+for i in sorted_dic:
+    for j in range(i[1]):
+        print(i[0], end=' ')
