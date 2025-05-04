3c41f12f7bafc6100e9c5a41975e382a1678d6d0
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-05-05 07:26:06 +0900

    [Silver IV] Title: DNA, Time: 76 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1969. DNA/DNA.py
@@ -0,0 +1,23 @@
+# T A G C
+n,m = map(int, input().split())
+arr = [list(input().strip()) for _ in range(n)]
+
+result = ''
+for i in range(m):
+    dic = {}
+    for j in range(n):
+        c = arr[j][i]
+        if c not in dic:
+            dic[c] = 1
+        elif c in dic:
+            dic[c] += 1
+    result += min(k for k,v in dic.items() if v == max(dic.values()))
+
+cnt  = 0
+for i in arr:
+    for j in range(len(i)):
+        if i[j] != result[j]:
+            cnt+=1
+
+print(result)
+print(cnt)
