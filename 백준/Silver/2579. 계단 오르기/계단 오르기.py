c1e592b8069b7f81277d0ce759583b463b9dd371
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-05-12 12:24:16 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,14 +1,14 @@
 n = int(input())
-st = [int(input()) for _ in range(n)]
-dp = [0 for _ in range(n + 1)]
+arr = [int(input()) for _ in range(n)]
+dp = [0] * max(n, 4)
 
+dp[0] = arr[0]
+if n >= 2:
+    dp[1] = arr[0]+arr[1]
+if n >= 3:
+    dp[2] = arr[2] + max(arr[0], arr[1])
+if n >= 4:
+    for i in range(3,n):
+        dp[i] = arr[i]+ max(dp[i-2], dp[i-3]+arr[i-1])
 
-def jump(n):
-    dp[1] = st[0]
-    if n >= 2:
-        dp[2] = st[0] + st[1]
-        for i in range(3, n + 1):
-            dp[i] = st[i - 1] + max(dp[i - 2], st[i - 2] + dp[i - 3])
-    return dp[n]
-
-print(jump(n))
+print(dp[n-1])
