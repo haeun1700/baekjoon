f2688c654723aa6799638432caa3aa05eac9b383
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-07-19 20:31:40 +0900

    [Bronze V] Title: 구구단, Time: 176 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,12 @@
-import java.io.IOException;
 import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 1; i < 10; i++){
-            System.out.printf("%d * %d = %d\n", n, i, n*i);
+public class Main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for(int i = 1; i<10; i++){
+            System.out.printf("%d * %d = %d\n", n,i,n*i);
         }
     }
 }
