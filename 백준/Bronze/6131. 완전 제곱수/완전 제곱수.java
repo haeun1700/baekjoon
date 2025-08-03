85ff4c8300e9c55b2dfc68e827ade406f47155d2
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-08-03 20:11:47 +0900

    [Bronze III] Title: 완전 제곱수, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for(int i = 1; i<= 500; i++){
+            int a = i;
+            for(int j = 1; j <= 500; j++){
+                if(a*a == j*j+n){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
