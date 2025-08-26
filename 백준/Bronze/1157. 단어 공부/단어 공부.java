66ba3796e17442da7952980e2188a3d2383e8907
haeun1700 <111607963+haeun1700@users.noreply.github.com>
2025-08-26 21:17:35 +0900

    [Bronze I] Title: 단어 공부, Time: 280 ms, Memory: 21136 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,43 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
-/*
-1. 단어를 입력받는다. -> 대문자로 처리한다.
-2. HashMap을 선언한다.
-    2-1. 해당 문자열의 문자를 key,value로 저장한다.
-3. Map의 키와 밸류값이 무엇인지 순회한다.
-    3-1. 만약 밸류가 현재 max보다 높으면 갱신
-    3-2. 만약 max와 value가 동일하면 ?로 저장
-4. 최종 res값 출력
- */
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        //1. 단어를 입력받는다. -> 대문자로 처리한다.
         String word = br.readLine().toUpperCase();
-        //2. HashMap을 선언한다.
-        HashMap<Character, Integer> map = new HashMap<>();
-        //2-1. 해당 문자열의 문자를 key,value로 저장한다.
-        for(int i = 0; i< word.length(); i++){
-            char c = word.charAt(i);
-            map.put(c, map.getOrDefault(c, 0)+1);
+        int[] alpha = new int[26];
+        for(int i = 0; i<word.length(); i++){
+            alpha[word.charAt(i)-'A']++;
         }
-        // 3. Map의 키와 밸류값이 무엇인지 순회한다.
         int maxValue = Integer.MIN_VALUE;
         char res = ' ';
+        for(int i = 0; i < word.length(); i++){
+            int diff = alpha[word.charAt(i)-'A'];
 
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-
-            if(maxValue <= entry.getValue()){
-                // 3-2. 만약 max와 value가 동일하면 ?로 저장
-                res = maxValue == entry.getValue() ? '?' : entry.getKey();
-                // 3-1. 만약 밸류가 현재 max보다 높으면 갱신
-                maxValue = entry.getValue();
+            if(diff >= maxValue){
+                if(diff == maxValue){
+                    res = word.charAt(i) == res ? res : '?';
+                }else{
+                    res = word.charAt(i);
+                }
+                maxValue = diff;
             }
         }
         System.out.println(res);
